e6b32438f3259f6810944a5b7a8f9768bbdaae74
Alexander J. Salas B <asalas@teclib.com>
2016-10-10 16:50:05 +0200

    Add throw errors

--- angular-glpi.js
+++ angular-glpi.js
@@ -18,13 +18,43 @@
         ListSearchOptions: "/listSearchOptions/",
         SearchItems: "/search/"
       };
-      String.prototype.toValidUrl = function () {
+      var errorMsg = {
+        invalid_url: [
+          'ERROR_INVALID_URL', ''],
+        invalid_itemtype: [
+          'ERROR_ITEM_NOT_FOUND', ''],
+        invalid_range: [
+          'ERROR_INVALID_RANGE', ''],
+      };
+      String.prototype.toConcatSlash = function () {
         var lastChar = this.substr(-1);
         if (lastChar !== '/') {
           return this + '/';
         }
         return this;
       };
+      function validURL(url) {
+        var pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
+          '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|' + // domain name
+          '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+          '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+          '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+          '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
+        return pattern.test(url);
+      }
+      function validItemType(itemtype) {
+        var itemtypes = ['User'];
+        var found = itemtypes.indexOf(itemtype);
+        if (found === -1) {
+          return false;
+        } else {
+          return true;
+        }
+      }
+      function validRange(range) {
+        var pattern = new RegExp('/^\d+-\d+|\*$/');
+        return pattern.test(range);
+      }
       return {
         initsession: function () { },
         killsession: function () { },
@@ -38,16 +68,27 @@
         getAnTtem: function () { },
         getAllItems: function (url, itemtype, range) {
           var getUsersDefer = $q.defer();
+          if (!validURL(url)) {
+            throw new Error(errorMsg.invalid_url);
+          }
+          if (!validItemType(itemtype)) {
+            throw new Error(errorMsg.invalid_url);
+          }
+          if (range) {
+            if (!validRange(range)) {
+              throw new Error(errorMsg.invalid_range);
+            }
+          }
           $http({
             method: 'GET',
-            url: url.toValidUrl() + itemtype,
+            url: url.toConcatSlash() + itemtype,
             params: {
               range: range ? range : maxRange
             }
           }).success(function (resp) {
             getUsersDefer.resolve(resp);
-          }).error(function () {
-            getUsersDefer.reject();
+          }).error(function (error) {
+            getUsersDefer.reject(error);
           });
           return getUsersDefer.promise;
         },
